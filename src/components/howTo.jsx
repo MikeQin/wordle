986b5dd8e90f9f9c615cb4432dadb4e5e97b95a3
MikeQin <mike.qin@gmail.com>
2022-06-19 02:43:14 -0500

    initial

--- src/components/howTo.jsx
+++ src/components/howTo.jsx
@@ -8,10 +8,20 @@ export default function HowTo({setShowHowTo, solution}) {
   return (
     <div className='panel'>
       <div>
-        <h1>How to Play?</h1>
-        <img src='/how-to-work.jpg' width="480" height={500} alt='how to play'/>
-        <br/>
-        <button type="button" onClick={handleOKButton}>OK</button>
+        <h1 style={{textAlign: "center"}}>How to Play?</h1>
+        <p>Wordle is such a simple game that there are hardly any rules. But here you go:<br/>
+        <ul>
+          <li>You have to guess the Wordle in six goes or less</li>
+          <li>Every word you enter must be in the word list. There are more than 10,000 words in this list, 
+            but only 2,309 (at the time of writing) are answers to a specific puzzle</li>
+          <li>A correct letter turns green</li>
+          <li>A correct letter in the wrong place turns yellow</li>
+          <li>An incorrect letter turns gray</li>
+          <li>Letters can be used more than once</li>
+          <li>Answers are never plurals</li>
+        </ul>
+        </p>
+        <p style={{textAlign: "center"}}><button type="button" onClick={handleOKButton}>OK</button></p>
         <p className='light'>{'build: 20220619'} {solution}</p>
       </div>
     </div>
